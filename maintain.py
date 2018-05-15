2c6e95d6b2945280f24c7eb09ba1f4ce5c0e809d
kysevenle <kysevenle@gmail.com>
2018-05-14 20:15:28 -0600

    added late fee check function to maintain file to check for overdue invoices with amount less than 15 dollars

--- maintain.py
+++ maintain.py
@@ -7,3 +7,9 @@ def check_for_service_notes_by_area(area_regex):
         for ip in service['ipRanges']:
             if re.match(area_regex, ip):
                 print(ip, service['note'])
+
+def late_fee_check():
+    invoices = wp2.api.calls.get_invoices('?overdue=1')
+    for invoice in invoices:
+        if invoice['total'] - invoice['amountPaid'] < 15:
+            print(invoice['clientId'])
