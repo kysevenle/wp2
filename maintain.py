140c18cd4d56ba80963fba13c392a6e2b52dfb89
kysevenle <kysevenle@gmail.com>
2018-05-14 21:28:31 -0600

    added function to get overdue invoices along with the number of days overdue

--- maintain.py
+++ maintain.py
@@ -1,5 +1,6 @@
 import wp2
 import re
+import datetime
 
 def check_for_service_notes_by_area(area_regex):
     services = wp2.api.calls.get_services()
@@ -13,3 +14,14 @@ def late_fee_check():
     for invoice in invoices:
         if invoice['total'] - invoice['amountPaid'] < 15:
             print(invoice['clientId'])
+
+def get_days_overdue():
+    invoices = wp2.api.calls.get_invoices('?overdue=1')
+    today = datetime.date.today()
+    for invoice in invoices:
+        due_date = invoice['dueDate'].split('T')
+        due_date = due_date[0]
+        year, month, day = due_date.split('-')
+        due_date = datetime.date(int(year), int(month), int(day))
+        days_overdue = today - due_date
+        print(str(days_overdue))
