d8cf39c45f0cfab28b4e95e53e5bd4f30f908267
kysevenle <kysevenle@gmail.com>
2018-05-17 10:01:53 -0600

    deleted blank line

--- maintain.py
+++ maintain.py
@@ -25,14 +25,3 @@ def late_fee_check():
                 'clientId': invoice['clientId'],
             }
             wp2.api.calls.create_job(payload)
-
-def get_days_overdue():
-    invoices = wp2.api.calls.get_invoices('?overdue=1')
-    today = datetime.date.today()
-    for invoice in invoices:
-        due_date = invoice['dueDate'].split('T')
-        due_date = due_date[0]
-        year, month, day = due_date.split('-')
-        due_date = datetime.date(int(year), int(month), int(day))
-        days_overdue = today - due_date
-        print(str(days_overdue))
