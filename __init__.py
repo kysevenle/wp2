6461fe64fcbf78f7f55770fc8fe6e2c309c6bae6
kysevenle <kysevenle@gmail.com>
2018-05-18 15:52:01 -0600

    created daily script to check for incorrect late fees, and services with both a balance and a credit

--- __init__.py
+++ __init__.py
@@ -2,3 +2,4 @@ import wp2.api
 from wp2.areas import areas
 import wp2.add_service_lines
 import wp2.maintain
+import wp2.daily
--- maintain.py
+++ maintain.py
@@ -9,23 +9,6 @@ def check_for_service_notes_by_area(area_regex):
             if re.match(area_regex, ip):
                 print(ip, service['note'])
 
-def late_fee_check():
-    invoices = wp2.api.calls.get_invoices('?overdue=1')
-    today = datetime.date.today()
-    for invoice in invoices:
-        due_date = invoice['dueDate'].split('T')
-        due_date = due_date[0]
-        year, month, day = due_date.split('-')
-        due_date = datetime.date(int(year), int(month), int(day))
-        days_overdue = today - due_date
-        if days_overdue == datetime.timedelta(days=16) and invoice['total'] - invoice['amountPaid'] <= 15:
-            payload = {
-                'title': 'Remove late fee',
-                'description': 'Remove late fee from account before invoices are processed',
-                'clientId': invoice['clientId'],
-            }
-            wp2.api.calls.create_job(payload)
-
 def get_late_fees():
     invoices = wp2.api.calls.get_invoices('?overdue=1')
     today = datetime.date.today()
