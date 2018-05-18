b8c804fec05a90d7f381e4f4631aa30d143f16b6
kysevenle <kysevenle@gmail.com>
2018-05-18 09:11:40 -0600

    added function to get list of late fees applied that day

--- maintain.py
+++ maintain.py
@@ -25,3 +25,15 @@ def late_fee_check():
                 'clientId': invoice['clientId'],
             }
             wp2.api.calls.create_job(payload)
+
+def get_late_fees():
+    invoices = wp2.api.calls.get_invoices('?overdue=1')
+    today = datetime.date.today()
+    for invoice in invoices:
+        due_date = invoice['dueDate'].split('T')
+        due_date = due_date[0]
+        year, month, day = due_date.split('-')
+        due_date = datetime.date(int(year), int(month), int(day))
+        days_overdue = today - due_date
+        if days_overdue == datetime.timedelta(days=16):
+            print(invoice['clientId'])
