3bb0e8f420d43a3067dcfe0f71ce80214c78b664
kysevenle <kysevenle@gmail.com>
2018-05-21 13:05:14 -0600

    finished lines script to get all IPs on a certain line

--- __init__.py
+++ __init__.py
@@ -3,3 +3,4 @@ from wp2.areas import areas
 import wp2.add_service_lines
 import wp2.maintain
 import wp2.daily
+import wp2.lines
--- lines.py
+++ lines.py
@@ -1,24 +1,23 @@
 import wp2
 
-devices = wp2.api.calls.get_devices()
+def main():
+    devices = wp2.api.calls.get_devices()
 
-interfaces = {}
+    interfaces = {}
 
-for device in devices:
-    device_ints = wp2.api.calls.get_device_ints(str(device['id']))
-    for interface in device_ints:
-        id = interface['id']
-        interfaces[id] = interface
-        interfaces[id]['clientIps'] = []
+    for device in devices:
+        device_ints = wp2.api.calls.get_device_ints(str(device['id']))
+        for interface in device_ints:
+            id = interface['id']
+            interfaces[id] = interface
+            interfaces[id]['deviceName'] = device['name']
+            interfaces[id]['clientIps'] = []
 
-services = wp2.api.calls.get_services()
+    services = wp2.api.calls.get_services()
 
-no_interface = []
+    no_interface = []
 
-for service in services:
-    if service['status'] == 2:
-        continue
-    else:
+    for service in services:
         service_devices = wp2.api.calls.get_service_devices(str(service['id']))
         for service_device in service_devices:
             interface_id = service_device['interfaceId']
@@ -28,3 +27,15 @@ for service in services:
             else:
                 for ip in service['ipRanges']:
                     interfaces[interface_id]['clientIps'].append(ip)
+
+    for interface, interface_info in interfaces.items():
+        if not interface_info['clientIps']:
+            continue
+        else:
+            with open(r'C:/Users/Kyle/Desktop/lines/' + interface_info['deviceName'] + '_' + interface_info['name'] + '.txt', 'w') as file:
+                for ip in interface_info['clientIps']:
+                    file.write(ip + '\n')
+
+
+if __name__ == "__main__":
+    main()
