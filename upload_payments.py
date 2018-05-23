e03dcc4dc6825125d9b45a3d0084d2cc7b8261b9
kysevenle <kysevenle@gmail.com>
2018-05-23 09:09:42 -0600

    started payment upload script

---
+++ upload_payments.py
@@ -0,0 +1,9 @@
+import csv
+import wp2
+import os
+
+for vb_file in os.listdir(r'C:\Users\Kyle\Desktop\vanco_batches'):
+    with open(r'C:\Users\Kyle\Desktop\vbatches\.csv') as file:
+        reader == csv.reader(file)
+        for row in reader:
+            print(row)
