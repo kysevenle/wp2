f616c7e067faf112689c79c3e6543690faa04161
kysevenle <kysevenle@gmail.com>
2018-05-25 13:17:49 -0600

    finished authorize batch upload functions

--- upload_payments.py
+++ upload_payments.py
@@ -12,6 +12,27 @@ def get_clients_as_dict():
         clients[id] = client
     return clients
 
+def auth_d_to_ucrm_d(auth_d):
+    date = auth_d.split()[0]
+    year = date.split('-')[2]
+    month = date.split('-')[1]
+    month = list(calendar.month_abbr).index(month)
+    day = date.split('-')[0]
+    if auth_d.split()[2] == 'PM':
+        time = auth_d.split()[1]
+        hour = int(time.split(':')[0]) + 12
+        minute = time.split(':')[1]
+        second = time.split(':')[2]
+        time = f"{hour}:{minute}:{second}"
+    else:
+        time = auth_d.split()[1]
+        hour = time.split(':')[0]
+        minute = time.split(':')[1]
+        second = time.split(':')[2]
+        time = f"{hour}:{minute}:{second}"
+    date = f"{year}-{month}-{day}T{time}-0600"
+    return date
+
 
 def upload_vanco_batches(clients):
     fieldnames = ['vancoId', 'vancoName', 'accountNumber', 'amount', 'datePaid', 'dateDeposited', 'unknown1', 'unknown2', 'referanceNumber', 'firstName', 'middleInitial', 'lastName', 'address', 'unknown3', 'city', 'state', 'zip']
@@ -30,7 +51,6 @@ def upload_vanco_batches(clients):
                     payload = {
                         'clientId': id,
                         'method': 99,
-                        'createdDate': payment_time,
                         'amount': round(amount, 2),
                         'currencyCode': 'USD',
                         'providerName': 'Vanco',
@@ -60,13 +80,19 @@ def upload_authorize_batches():
                     else:
                         id = clients[account_number]['id']
                     amount = float(row['Total Amount'])
-                    date = row['Submit Date/Time'].split()[0]
-                    year = date.split('-')[2]
-                    month = date.split('-')[1]
-                    month = list(calendar.month_abbr).index(month)
-                    day = date.split('-')[0]
-                    date = f"{year}-{month}-{day}"
-                    print(date)
+                    date = auth_d_to_ucrm_d(row['Submit Date/Time'])
+                    payload = {
+                        'clientId': id,
+                        'method': 9,
+                        'createdDate': date,
+                        'amount': round(amount, 2),
+                        'currencyCode': 'USD',
+                        'note': 'Authorize.net Transaction - ' + row['Transaction ID'],
+                        'applyToInvoicesAutomatically': True,
+                    }
+                    print(row['Transaction ID'] + ' - ' + row['Customer First Name'] + ', ' + row['Customer Last Name'])
+                    wp2.api.calls.create_payment(payload)
+            os.rename(file.path, r"C:\Users\Kyle\Dropbox\Authorize Batches Archive" + '\\' + file.name)
 
 def main():
     clients = get_clients_as_dict()
