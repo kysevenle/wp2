a0b3e35bb8d567af36774bd1c5f11e0cbc236a3b
kysevenle <kysevenle@gmail.com>
2018-05-31 14:51:26 -0600

    created remove signal stats function

--- maintain.py
+++ maintain.py
@@ -1,6 +1,7 @@
 import wp2
 import re
 import datetime
+import json
 
 def check_for_service_notes_by_area(area_regex):
     services = wp2.api.calls.get_services()
@@ -20,3 +21,18 @@ def get_late_fees():
         days_overdue = today - due_date
         if days_overdue == datetime.timedelta(days=16):
             print(invoice['clientId'])
+
+def remove_signal_stats():
+    services = wp2.api.calls.get_services()
+    for service in services:
+        if service['status'] == 2:
+            continue
+        else:
+            service_devices = wp2.api.calls.get_service_devices(str(service['id']))
+            for service_device in service_devices:
+                if service_device['createSignalStatistics'] == False:
+                    continue
+                else:
+                    id = str(service_device['id'])
+                    payload = {'createSignalStatistics': False}
+                    response = wp2.api.calls.update_service_device(id, payload)
