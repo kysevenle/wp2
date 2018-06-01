86de6c16a628bf9cc3694b86c74490c2f960b4f9
kysevenle <kysevenle@gmail.com>
2018-06-01 15:03:33 -0600

    moved late fee check and credit balance check from daily.py to maintain.py to start building a daily script to run

--- daily.py
+++ daily.py
@@ -1,40 +1,10 @@
 import wp2
 import datetime
 
-def late_fee_check(days_late=16):
-    invoices = wp2.api.calls.get_invoices('?overdue=1')
-    today = datetime.date.today()
-    for invoice in invoices:
-        due_date = invoice['dueDate'].split('T')
-        due_date = due_date[0]
-        year, month, day = due_date.split('-')
-        due_date = datetime.date(int(year), int(month), int(day))
-        days_overdue = today - due_date
-        if days_overdue == datetime.timedelta(days_late) and invoice['total'] - invoice['amountPaid'] <= 15:
-            payload = {
-                'title': 'Remove late fee',
-                'description': 'Remove late fee from account before invoices are processed',
-                'clientId': invoice['clientId'],
-            }
-            wp2.api.calls.create_job(payload)
-
-def credit_balance_check():
-    clients = wp2.api.calls.get_clients()
-    for client in clients:
-        if client['accountOutstanding'] != 0 and client['accountCredit'] != 0:
-            if round(client['accountBalance'], 2) == round(client['accountCredit'], 2):
-                continue
-            else:
-                payload = {
-                    'title': 'Apply credit to invoices',
-                    'description': "Apply account's credit to account's invoices",
-                    'clientId': client['id'],
-                }
-                wp2.api.calls.create_job(payload)
 
 def main():
-    late_fee_check()
-    credit_balance_check()
+    wp2.maintain.late_fee_check()
+    wp2.maintain.credit_balance_check()
 
 if __name__ == '__main__':
     main()
--- maintain.py
+++ maintain.py
@@ -22,6 +22,37 @@ def get_late_fees():
         if days_overdue == datetime.timedelta(days=16):
             print(invoice['clientId'])
 
+def late_fee_check(days_late=15):
+    invoices = wp2.api.calls.get_invoices('?overdue=1')
+    today = datetime.date.today()
+    for invoice in invoices:
+        due_date = invoice['dueDate'].split('T')
+        due_date = due_date[0]
+        year, month, day = due_date.split('-')
+        due_date = datetime.date(int(year), int(month), int(day))
+        days_overdue = today - due_date
+        if days_overdue == datetime.timedelta(days_late) and invoice['total'] - invoice['amountPaid'] <= 15:
+            payload = {
+                'title': 'Remove late fee',
+                'description': 'Remove late fee from account before invoices are processed',
+                'clientId': invoice['clientId'],
+            }
+            wp2.api.calls.create_job(payload)
+
+def credit_balance_check():
+    clients = wp2.api.calls.get_clients()
+    for client in clients:
+        if client['accountOutstanding'] != 0 and client['accountCredit'] != 0:
+            if round(client['accountBalance'], 2) == round(client['accountCredit'], 2):
+                continue
+            else:
+                payload = {
+                    'title': 'Apply credit to invoices',
+                    'description': "Apply account's credit to account's invoices",
+                    'clientId': client['id'],
+                }
+                wp2.api.calls.create_job(payload)
+
 def remove_signal_stats():
     services = wp2.api.calls.get_services()
     for service in services:
